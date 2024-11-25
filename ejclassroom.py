506196b7c98c17c8b1b939699403b43a5921d017
Paola999-ic <pcaicedom@unal.edu.co>
2024-11-25 10:15:56 -0500

    Add files via upload
    es el primer codigo de la clase de programacion, quedo bonito

---
+++ ejclassroom.py
@@ -0,0 +1,15 @@
+a=2
+b=3
+c=5.4
+resultado1 = 2*a+3*b-c
+resultado2 = (0.25*a)+(3*b)-(0.75*c)
+resultado3 =((a+3*b-0.75*c)/3 + (a+b))
+print('a=2')
+print('b=3')
+print('c=5.4')
+print('1. (2a+3b-c)')
+print('2. (0.25a+3b-0.75c)')
+print('3. (a+3b-0.75c)/3 +(a+b)')
+print('El primer resultado fue de:', resultado1)
+print('El segundo resultado fue de:', resultado2)
+print('El tercer resultado fue de:' , resultado3)
